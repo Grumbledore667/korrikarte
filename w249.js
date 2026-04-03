202f3a77a3204fd91e961ba7b5148b8e985a1506
Wasserlan <PhillipBuehler@gmx.de>
2026-04-03 15:08:16 +0200

    Update w249.js

--- w249.js
+++ w249.js
@@ -128,7 +128,6 @@ win.$.getScript("https://shinko-to-kuma.com/scripts/mapSdk.js").done(() => {
         [382, 449, "wotan"],
         [382, 440, "Doldi"],
         [348, 389, "Wolfshirn"],
-        [326, 389, "xBlanusx"],
         [365, 420, "Tivanofaufuv Vex"],
         [282, 509, "???"],
         [335, 499, "Stromlan"],
